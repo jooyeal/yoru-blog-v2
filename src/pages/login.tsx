bed51c90ac16418df511f1d725c4d8d039219e25
jooyeal <34053954+jooyeal@users.noreply.github.com>
2023-03-15 21:19:02 +0900

    ADD: Login

---
+++ src/pages/login.tsx
@@ -0,0 +1,21 @@
+import { Center, Heading } from "@chakra-ui/react";
+import { GetServerSideProps, NextPage } from "next";
+import { getSession, signIn, signOut, useSession } from "next-auth/react";
+import React from "react";
+import Button from "~/components/molecules/Button";
+
+const login: NextPage = () => {
+  const { data: session } = useSession();
+  return (
+    <Center className="h-screen flex-col gap-12">
+      <Heading>Login page for admin</Heading>
+      {session ? (
+        <Button onClick={() => signOut()}>SIGNOUT</Button>
+      ) : (
+        <Button onClick={() => signIn()}>SIGNIN</Button>
+      )}
+    </Center>
+  );
+};
+
+export default login;
--- src/server/auth.ts
+++ src/server/auth.ts
@@ -5,6 +5,7 @@ import {
   type DefaultSession,
 } from "next-auth";
 // import DiscordProvider from "next-auth/providers/discord";
+import CredentialProvider from "next-auth/providers/credentials";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { env } from "~/env.mjs";
 import { prisma } from "~/server/db";
@@ -37,16 +38,42 @@ declare module "next-auth" {
  */
 export const authOptions: NextAuthOptions = {
   callbacks: {
-    session({ session, user }) {
-      if (session.user) {
-        session.user.id = user.id;
-        // session.user.role = user.role; <-- put other properties on the session here
-      }
+    async session({ session, user }) {
+      // if (session.user) {
+      //   session.user.id = user.id;
+      //   // session.user.role = user.role; <-- put other properties on the session here
+      // }
       return session;
     },
   },
-  adapter: PrismaAdapter(prisma),
   providers: [
+    CredentialProvider({
+      name: "Credentials",
+      // `credentials` is used to generate a form on the sign in page.
+      // You can specify which fields should be submitted, by adding keys to the `credentials` object.
+      // e.g. domain, username, password, 2FA token, etc.
+      // You can pass any HTML attribute to the <input> tag through the object.
+      credentials: {
+        email: { label: "Email", type: "text" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials, req) {
+        // Add logic here to look up the user from the credentials supplied
+        const user = await prisma.account.findFirst({
+          where: { email: credentials?.email, password: credentials?.password },
+        });
+
+        if (user) {
+          // Any object returned will be saved in `user` property of the JWT
+          return user;
+        } else {
+          // If you return null then an error will be displayed advising the user to check their details.
+          return null;
+
+          // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
+        }
+      },
+    }),
     // DiscordProvider({
     //   clientId: env.DISCORD_CLIENT_ID,
     //   clientSecret: env.DISCORD_CLIENT_SECRET,
